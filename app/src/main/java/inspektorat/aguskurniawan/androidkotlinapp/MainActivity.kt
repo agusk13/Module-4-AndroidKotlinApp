6f022afcce108cd627ff64a6c7fd11a2095beb29
agusk13 <agusk1308@gmail.com>
2025-11-02 12:20:04 +0700

    menambahkan tombol Add Number

--- app/src/main/java/inspektorat/aguskurniawan/androidkotlinapp/MainActivity.kt
+++ app/src/main/java/inspektorat/aguskurniawan/androidkotlinapp/MainActivity.kt
@@ -1,20 +1,31 @@
 package inspektorat.aguskurniawan.androidkotlinapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvCounter: TextView
+    private lateinit var btnAddNumber: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        tvCounter = findViewById(R.id.tvCounter)
+        btnAddNumber = findViewById(R.id.btnAddNumber)
+
+        tvCounter.text = "1"
+    }
+
+    fun addNumber(v: View){
+        val currVal = tvCounter.text.toString().toInt()
+        val nextVal = currVal + 1
+        tvCounter.text = nextVal.toString()
     }
 }
